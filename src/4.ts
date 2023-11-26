d18d36e69bc6de6551afd6493ecab5040a27fbfe
Igor2109 <127054357+Igor2109@users.noreply.github.com>
2023-11-26 18:30:29 +0100

    ts3

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,56 @@
-const key = new Key();
+interface IKey {
+  getSignature: () => number;
+}
+
+class Key {
+  constructor(private signature: number) {
+    this.signature = signature;
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+interface IPerson {
+  getKey: () => IKey;
+}
+
+class Person {
+  private key: IKey;
+  constructor(key: IKey) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  key: IKey;
+  door: boolean;
+  tenants: IPerson[];
+  comeIn(person: IPerson) {
+    if (this.door === true) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: IKey): void;
+}
+
+class MyHouse extends House {
+  door = false;
+  tenants = [];
+  constructor(key: IKey) {
+    super();
+    this.key = key;
+  }
+  openDoor(key: IKey) {
+    if (this.key === key) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +58,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
